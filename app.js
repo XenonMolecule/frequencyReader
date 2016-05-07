69ef223ca57c592fe39378be998566f6fcb56f2b
xenonmolecule <michael_ryan_2000@yahoo.com>
2016-05-07 17:24:52 +0000

    added array print option

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // JavaScript File
 var data;
+var midiFile;
 
 var openFile = function(event) {
     var input = event.target;
@@ -8,16 +9,31 @@ var openFile = function(event) {
     reader.onload = function(){
         var data = reader.result;
         console.log(data)
-        var midiFile = new MIDIFile(data);
+        midiFile = new MIDIFile(data);
         var events = midiFile.getMidiEvents();
         console.log(events);
+        var arrayOutput = "[";
+        $(".output").append("<li><p>Ticks Per Beat: "+midiFile.header.getTicksPerBeat()+"</p></li>")
         for(var i = 0; i < events.length; i++){
-            if(events[i].delta){
-                console.log("playTone("+events[i].param1+","+events[i].delta+")");
-                $(".output").append("<li><p>playTone("+events[i].param1+","+events[i].delta+")</p></li>")
+            if(events[i].subtype==04){
+                console.log(events[i]);
             }
+            if(events[i].delta){
+                if($('input[name=outputType]:checked', '#outputType').val()==="playTone"){
+                    //console.log("playTone("+events[i].param1+","+events[i].delta+");");
+                    $(".output").append("<li><p>["+events[i].playTime+"]playTone("+events[i].param1+","+Math.round(events[i].delta/10)+")</p></li>")
+            
+                } else {
+                    arrayOutput+="["+events[i].param1+","+Math.round(events[i].delta/10)+","+events[i].playTime+"],";
+                }
+            }
+        }
+        arrayOutput = arrayOutput.slice(0,-1);
+        arrayOutput+="]";
+        if($('input[name=outputType]:checked', '#outputType').val()==="array"){
+            $(".output").append("<li><p>"+arrayOutput+"</p></li>")
+            console.log(arrayOutput);
         }
     };
     reader.readAsArrayBuffer(input.files[0]);
 };
-
